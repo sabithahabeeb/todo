cc34054928b38bcfedc1a67ff956c8d0c4ca3dc9
sabithahabeeb <sabithahabeeb.sabi@gmail.com>
2023-10-28 15:56:35 +0530

    save arezzo files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id='body'style={{width:'100%',height:'100vh'}} className='flex justify-center items-center'>
+     
+       <div className='flex flex-col justify-center w-4/5 h-1/2 border-double border-4 border-indigo-600  items-center static'>
+          <h1 className='text-4xl bg-slate-200 m-6 w-96 h-14 text-center font-bold rounded-3xl text-rose-700 tracking-normal'>TODO LIST</h1>
+          <input className='w-1/2 h-14 rounded-full' type="text"  /><i className="fa-solid fa-plus absolute top-96 mt-5 bg-white rounded-full w-9 h-9 text-center font-bold text-xl font-bold bg-red-500 text-gray-50 right-1/3 ml-9  "></i>
+       
+       </div>
+      
     </div>
+    </>
   );
 }
 
